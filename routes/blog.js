a54816982a2b1b3e75425d70779620612a8111a0
Its Ya Boi Del Rey <delreeyyy@gmail.com>
2022-07-05 10:01:34 +0800

    CREATE and FETCH data from db to display in a blog

--- routes/blog.js
+++ routes/blog.js
@@ -2,19 +2,46 @@ const express = require('express');
 
 const db = require('../data/database.js');
 
-const route = express.Router();
+const router = express.Router();
 
-route.get('/', function(req, res){
+router.get('/', function(req, res){
     res.redirect('/posts');
 });
 
-route.get('/posts', function(req, res){
-    res.render('posts-list');
+router.get('/posts', async function(req, res){
+    const [posts] = await db.query('SELECT posts.*, authors.author_name FROM posts INNER JOIN authors ON posts.author_id = authors.author_id;');
+    res.render('posts-list', {posts: posts});
 });
 
-route.get('/new-post', async function(req, res){
+router.get('/new-post', async function(req, res){
     const [authors] = await db.query('SELECT * FROM authors;');
     res.render('create-post', {authors: authors});
 });
 
-module.exports = route;
+router.post('/posts', async function(req, res){
+    const data = [
+        req.body.title,
+        req.body.summary,
+        req.body.content,
+        req.body.author
+    ]
+    
+    await db.query('INSERT INTO posts (post_title, post_summary, post_body, author_id) VALUES (?)', [data]);
+
+    res.redirect('/posts');
+});
+
+router.get('/view-post/:post_id', async function(req, res){
+    
+    const query = `
+    SELECT posts.*, authors.author_name, authors.author_email FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.author_id
+    WHERE posts.post_id = ?
+    `;
+    
+    const [posts] = await db.query(query, req.params.post_id);
+    
+    res.render('post-detail', {post: posts[0]});
+});
+
+module.exports = router;
